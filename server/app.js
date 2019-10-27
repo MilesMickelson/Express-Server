9aeb25fc3e29cbe0540e6f3de2d2ad4035cf83e0
Miles Mickelson <milesmickelson87@gmail.com>
2019-10-27 12:04:59 -0700

    project complete with successful call to local data.json in the backend

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
-// import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data');
 
+const app = express();
+var requestCount = 0;
 
-// create your express server below
-var app;
+app.use(morgan('dev'));
 
-// add your routes and middleware below
+app.use(function(req, res, next){
+  requestCount++
+  console.log('request number: '+ requestCount);
+  next();
+});
 
-// finally export the express application
+app.get('/', function( req, res) {
+  res.status(200).send('Hello World');
+});
+
+app.get('/data', function (req, res) {
+  res.status(200).send(data);
+})
+
+app.get('*', function( req, res){
+  res.status(404).send('Sorry, no page found');
+});
+
 module.exports = app;
